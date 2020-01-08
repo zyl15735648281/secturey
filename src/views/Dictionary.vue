5f19d3fa082c2bfe308397df651b148faa66b3ca
zyl <1352843612@qq.com>
2020-01-08 22:29:16 +0800

    添加字典

---
+++ src/views/Dictionary.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     <p>字典管理</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dictionary",
+  components: {},
+  data () {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="less">
+</style>
